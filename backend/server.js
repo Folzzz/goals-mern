396ad247686ba40e40f9b043d8e86d7ab074b112
Folzzz <olaniyanfoluso498@gmail.com>
2022-12-13 05:34:16 +0100

    additional checks in server js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const colors = require('colors');
@@ -23,6 +24,16 @@ app.use(express.json());
 // ROUTES
 app.use('/api', goalRoutes);
 app.use('/api/users', userRoutes);
+
+// DEPLOYMENT PHASE - SERVE FRONTEND
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')));
+}
+else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+}
 
 // ERRORHANDLER MIDDLEWARE
 app.use(errorHandler);
